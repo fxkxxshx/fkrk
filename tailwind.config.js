c35e177415d348cab9c09d1093ae29d580e3f949
fkxsh <fxkxxshx@gmail.com>
2023-02-09 23:15:11 +0900

    Tailwind CSS カラー・フォントのカスタマイズ

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fkrk: {
+          purple: '#A11DF1',
+          gray: '#D7D7D7',
+        },
+      },
+      fontFamily: {
+        notoSansJp: ['Noto Sans JP'],
+        ibmPlexSans: ['IBM Plex Sans'],
+      },
+    },
   },
   plugins: [],
 };
